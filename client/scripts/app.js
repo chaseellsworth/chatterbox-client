f6f946acf3efbc98f38b7844a33d0ec0758d055a
chaseme3 <chaseme3@gmail.com>
2014-11-10 19:37:44 -0800

    added username and ability to post message

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,66 +1,117 @@
-$(document).ready(function(){
+var chatRoom = function() {
+  this.username;
+};
 
-  // var message = {
-  //   'username': 'shawndrost',
-  //   'text': 'trololo',
-  //   'roomname': '4chan'
-  // };
-  function htmlEscape(str) {
-    return String(str)
+chatRoom.prototype.getMessages = function() {
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'GET',
+    data: {order: '-createdAt', limit: 50},
+    contentType: 'application/json',
+    success: function (data) {
+      // console.log("Request for data SUCCESSFUL");
+      // console.log(data);
+      var messages = data.results;
+
+      $('#messages').empty();
+
+      for (var i = 0; i < messages.length; i++) {
+        var message = messages[i];
+        var text = chatRoom.prototype.htmlEscape(message.text);
+        var user = chatRoom.prototype.htmlEscape(message.username);
+        var time = chatRoom.prototype.htmlEscape(message.createdAt);
+        var room = chatRoom.prototype.htmlEscape(message.roomname);
+
+        var $message = $('<div>' +
+          '<p>' + user +'</p>' +
+          '<p>' + text +'</p>' +
+          '<p>' + time +'</p>' +
+          '<p>' + room +'</p>' +
+          '</div>');
+
+        $('#messages').append($message);
+      }
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Request for data FAILED');
+    }
+  });
+};
+
+chatRoom.prototype.htmlEscape = function(str) {
+  return String(str)
             .replace(/&/g, '&amp;')
             .replace(/"/g, '&quot;')
             .replace(/'/g, '&#39;')
             .replace(/</g, '&lt;')
             .replace(/>/g, '&gt;');
- };
+};
 
-  var getMessages = function(){
-    $.ajax({
-    // always use this url
-      url: 'https://api.parse.com/1/classes/chatterbox',
-      type: 'GET',
-      // data: JSON.stringify(message),
-      contentType: 'application/json',
-      success: function (data) {
-        // console.log("Request for data SUCCESSFUL");
-        // console.log(data);
-        var messages = data.results;
+chatRoom.prototype.formatMessage = function(message){
 
-        $('#messages').empty();
-
-        for (var i = 0; i < messages.length; i++) {
-          var message = messages[i];
-          var text = htmlEscape(message.text);
-          var user = htmlEscape(message.username);
-          var time = htmlEscape(message.createdAt);
-          var room = htmlEscape(message.roomname);
-
-          var $message = $('<div>' +
-            '<p>' + user +'</p>' +
-            '<p>' + text +'</p>' +
-            '<p>' + time +'</p>' +
-            '<p>' + room +'</p>' +
-            '</div>');
-
-          $('#messages').append($message);
-        }
-      },
-      error: function (data) {
-        // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-        console.error('chatterbox: Request for data FAILED');
-      }
-    });
+  var formattedMessage = {
+  'username':  this.username,
+  'text':  message,
+  'roomname': 'door'
   };
+  this.postMessage(formattedMessage);
+};
 
-  getMessages();
-  // setInterval(getMessages, 3000);
+chatRoom.prototype.postMessage = function(message){
+  $.ajax({
+    // always use this url
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+
+
+$(document).ready(function(){
+
+  var chatterbox = new chatRoom;
+  chatterbox.getMessages();
+
+
+  // setInterval(chatterbox.getMessages, 3000);
   $("button").on("click", function(){
-    getMessages();
+    chatterbox.getMessages();
   });
 
-  // console.log(messages);
+$("#user_name").on("focusout",function(e){
+  e.preventDefault();
+  chatterbox.username = $("#user_name").val();
+
+});
+
+$("#new_message").keypress(function(e){
+  if (e.which === 13 || e.keycode === 13){
+    e.preventDefault();
+    var newMessage = $("#new_message").val();
+    $("#new_message").val("");
+    chatterbox.formatMessage(newMessage);
+    chatterbox.getMessages();
+
+  }
+});
+
+//////////////////USERNAME
 
 
+//////////////////POST
+
+//////////////////POST
 
 
 
